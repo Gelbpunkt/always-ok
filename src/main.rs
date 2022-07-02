da3998822d338328231e4a210b414cecdc861675
Jens Reidel <adrian@travitia.xyz>
2022-07-02 17:27:33 +0200

    Make it smaller

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@
 use std::{
     io::{BufReader, Read, Result, Write},
     net::{SocketAddr, TcpListener, TcpStream},
-    thread::available_parallelism,
+    thread::{available_parallelism, spawn},
 };
-
-mod threadpool;
 
 const PORT: u16 = 8080;
 const BUFFER_SIZE: usize = 8096;
@@ -16,15 +14,25 @@ const OK: &[u8] = b"HTTP/1.1 200 OK\r\n";
 
 #[no_mangle]
 pub fn main(_argc: i32, _argv: *const *const u8) {
-    let pool = threadpool::ThreadPool::new(available_parallelism().unwrap());
-
     let addr = SocketAddr::from(([0, 0, 0, 0], PORT));
     let listener = TcpListener::bind(addr).unwrap();
 
-    for stream in listener.incoming() {
-        if let Ok(stream) = stream {
-            pool.spawn(move || handle(stream));
-        }
+    let thread_count = available_parallelism().unwrap();
+    let mut threads = Vec::with_capacity(thread_count.get());
+
+    for _ in 0..thread_count.get() {
+        let listener = listener.try_clone().unwrap();
+        threads.push(spawn(move || listen_task(listener)))
+    }
+
+    for thread in threads {
+        thread.join().unwrap();
+    }
+}
+
+fn listen_task(listener: TcpListener) {
+    for stream in listener.incoming().flatten() {
+        let _ = handle(stream);
     }
 }
 
